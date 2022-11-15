340e9cafdbc937b26a72cdbd5ed7d447566aa9f9
yoonsun9128 <yoonsun9128@gmail.com>
2022-11-15 09:29:50 +0900

    최윤선 아침 퀴즈

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from users.models import User
 # Create your tests here.
+class UserRegistrationAPITestCase(APITestCase):
+    def test_registration(self):
+        url = reverse("users:signup")
+        user_data = {
+            "username":"yoonsun",
+            "password":"123",
+            "email":"test@test.com",
+            "fullname":"choiyoonsun"
+        }
+        response = self.client.post(url, user_data)
+        print(response.data)
+        self.assertEqual
--- users/urls.py
+++ users/urls.py
@@ -5,11 +5,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+app_name="users"
+
 
 urlpatterns = [
     # user/
-    path('', views.UserView.as_view()),
+    path('', views.UserView.as_view(), name="signup"),
     path('api/token/', views.SpartaTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
- 
+
 ]
